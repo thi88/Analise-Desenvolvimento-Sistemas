f92ea05375ad6fdfcb8e781abda37e68ccf9708b
Thiago Mendonça <thiagomendoncadossantos@uni9.edu.br>
2021-10-17 20:32:42 -0300

    primeiro commit da aula 02

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <h1>MESA DA GORDINHA</h1>
+  <?php
+   echo "Ela não esperou a minha!!!<br>";
+   echo "Agora vou vender pra Deda hahahaha<br>";
+   ?>
+</body>
+</html>
